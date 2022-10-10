d2b54b7d8b052b151505883ef8bc622a26dc460a
Allan Jardine <allan@datatables.net>
2022-10-10 19:48:48 +0000

    Add support for mjs files (ES Modules)

---
+++ js/colReorder.bootstrap4.mjs
@@ -0,0 +1,13 @@
+
+/*! Bootstrap 4 styling wrapper for ColReorder
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs4';
+import DataTable from 'datatables.net-colreorder';
+
+
+
+
+export default DataTable;
