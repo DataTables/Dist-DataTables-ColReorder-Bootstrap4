ce6db8434a10abd2866b1d5df556481f117eb517
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:10:06 +0000

    b03ec4b81a6e3d607437a6c23649032b63740d17
    Sync to source repo @03ec4b81a6e3d607437a6c23649032b63740d17

--- js/colReorder.bootstrap4.js
+++ js/colReorder.bootstrap4.js
@@ -28,10 +28,9 @@
 				require('datatables.net-bs4')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.ColReorder ) {
 				require('datatables.net-colreorder')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
